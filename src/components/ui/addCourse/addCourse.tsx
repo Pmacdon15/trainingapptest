5731430f53027fb37eab977ddf88200c2be0370f
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-09-02 16:29:26 -0600

    changed font color

--- src/components/ui/addCourse/addCourse.tsx
+++ src/components/ui/addCourse/addCourse.tsx
@@ -1,10 +1,9 @@
 'use client';
-import { addCourse} from "@/actions/actions";
+import { addCourse } from "@/actions/actions";
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button";
 
-export default function AddCourse({ dayOfTraining , closeAddCourseFunction }: { dayOfTraining : number ,closeAddCourseFunction: Function }) {
-    // console.log(dayOfTraining);
+export default function AddCourse({ dayOfTraining, closeAddCourseFunction }: { dayOfTraining: number, closeAddCourseFunction: Function }) {
     const bindFormWithDayOfTraining = addCourse.bind(null, dayOfTraining);
 
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
@@ -14,7 +13,8 @@ export default function AddCourse({ dayOfTraining , closeAddCourseFunction }: { 
         bindFormWithDayOfTraining(formData);
         closeAddCourseFunction();
     }
-    
+    //TODO: Add form state to disable button when clicked
+
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
             <div className="h-2/6 w-5/6 md:w-2/6 bg-[#363f4e] p-4 rounded shadow">
